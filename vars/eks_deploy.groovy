93f4d798a7554697da7ad32e5cce418d38860914
Bart Labno <labno.b@gmail.com>
2019-06-04 13:48:37 +0100

    init commit for k8s in libs

--- vars/eks_deploy.groovy
+++ vars/eks_deploy.groovy
@@ -26,5 +26,23 @@ def call(Map buildParams) {
                 --alb-ingress-access \
                 ${buildParams.eksParams}""", label: "create cluster if not exist"
         }
+        stage("deploy ${buildParams.env}") {
+            sh script: "(kubectl describe service/${projectName}-service | grep role=green -c && export role=blue && export alterRole=green) || (export role=green && export alterRole=blue)"
+            sh script: "echo \"ProjectName: ${projectName}\" >> ./infrastructure/k8s/values.yaml", label: "building helm values - project name"
+            sh script: "echo \"Env: ${buildParams.env}\" >> ./infrastructure/k8s/values.yaml", label: "building helm values - environment"
+            sh script: "echo \"AwsRegion: ${awsRegion}\" >> ./infrastructure/k8s/values.yaml", label: "building helm values - AWS region"
+            sh script: "echo \"BranchName: ${$BRANCH_NAME}\" >> ./infrastructure/k8s/values.yaml", label: "building helm values - branch name"
+            sh script: "echo \"BuildNumber: ${BUILD_NUMBER}\" >> ./infrastructure/k8s/values.yaml", label: "building helm values - build number"
+            sh script: "echo \"Role: ${role}\" >> ./infrastructure/k8s/values.yaml", label: "building helm values - role"
+            sh script: "echo \"AlterRole: ${alterRole}\" >> ./infrastructure/k8s/values.yaml", label: "building helm values - alter role"
+            sh script: "cat ./infrastructure/k8s/values.yaml"
+        }
+        stage("integration tests ${buildParams.env}") {
+            sh "sleep 10"
+        }
+        stage("promote ${buildParams.env}") {
+            sh script: "sed -i \"s/AlterRole.*/AlterRole: ${role}/g\" ./infrastructure/k8s/values.yaml", label: "building helm values - alter role swap"
+            sh script: "cat ./infrastructure/k8s/values.yaml"
+        }
     }
 }
