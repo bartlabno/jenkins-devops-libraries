5ccafea4f3b88bdb12b975748593f7aae597f437
Bart Labno <labno.b@gmail.com>
2019-06-06 14:34:14 +0100

    yaml vars test

--- vars/docker_builder.groovy
+++ vars/docker_builder.groovy
@@ -7,29 +7,27 @@ def call(Map buildParams) {
             def defaults_file = findFiles(glob: '**/infrastructure/jenkins/defaults.y?ml')
             def defaults = readYaml file: "${defaults_file[0].path}"
             defaults.each { item -> 
-                echo """okay or not ${item}"""
+                echo """${item}"""
             }    
-        
-        stage('test') {
-            sh script: "docker build --no-cache -t ${defaults.projectName}-test -f Dockerfile.test .", label: "build test docker image"
-            sh script: "docker run ${defaults.projectName}-test", label: "run test docker image"
-        }
-        stage('build') {
-            parallel (
-                "image build": { sh script: "docker build --no-cache -t ${defaults.projectName} -f Dockerfile .", label: "build image" },
-                "registry login": { sh script: "\$(aws ecr get-login --region ${defaults.awsRegion} --no-include-email)", label: "login to docker registry" }
-            )
-        }
-        stage('publish') {
-            sh script: "docker tag ${defaults.projectName} \$(aws sts get-caller-identity | jq -r .Account).dkr.ecr.${defaults.awsRegion}.amazonaws.com/${defaults.projectName}/${defaults.projectName}:\$(echo $BRANCH_NAME)-\$(echo $BUILD_NUMBER)", label: "tag image"
-            sh script: "(aws ecr list-images --region ${defaults.awsRegion} --repository-name ${defaults.projectName}/${defaults.projectName}) || (aws ecr create-repository --region ${defaults.awsRegion} --repository-name ${defaults.projectName}/${defaults.projectName})", label: "check if repository exist"
-            sh script: "docker push \$(aws sts get-caller-identity | jq -r .Account).dkr.ecr.${defaults.awsRegion}.amazonaws.com/${defaults.projectName}/${defaults.projectName}:\$(echo $BRANCH_NAME)-\$(echo $BUILD_NUMBER)", label: "push image to registry"
-            if (env.BRANCH_NAME == "sharedlibs") {
-                sh "echo okokokokook"
-                sh script: "docker tag ${defaults.projectName} \$(aws sts get-caller-identity | jq -r .Account).dkr.ecr.${defaults.awsRegion}.amazonaws.com/${defaults.projectName}/${defaults.projectName}:latest", label: "tag image latest"
-                sh script: "docker push \$(aws sts get-caller-identity | jq -r .Account).dkr.ecr.${defaults.awsRegion}.amazonaws.com/${defaults.projectName}/${defaults.projectName}:latest", label: "push latest image to registry"
+            stage('test') {
+                sh script: "docker build --no-cache -t ${defaults.projectName}-test -f Dockerfile.test .", label: "build test docker image"
+                sh script: "docker run ${defaults.projectName}-test", label: "run test docker image"
             }
-        }
+            stage('build') {
+                parallel (
+                    "image build": { sh script: "docker build --no-cache -t ${defaults.projectName} -f Dockerfile .", label: "build image" },
+                    "registry login": { sh script: "\$(aws ecr get-login --region ${defaults.awsRegion} --no-include-email)", label: "login to docker registry" }
+                )
+            }
+            stage('publish') {
+                sh script: "docker tag ${defaults.projectName} \$(aws sts get-caller-identity | jq -r .Account).dkr.ecr.${defaults.awsRegion}.amazonaws.com/${defaults.projectName}/${defaults.projectName}:\$(echo $BRANCH_NAME)-\$(echo $BUILD_NUMBER)", label: "tag image"
+                sh script: "(aws ecr list-images --region ${defaults.awsRegion} --repository-name ${defaults.projectName}/${defaults.projectName}) || (aws ecr create-repository --region ${defaults.awsRegion} --repository-name ${defaults.projectName}/${defaults.projectName})", label: "check if repository exist"
+                sh script: "docker push \$(aws sts get-caller-identity | jq -r .Account).dkr.ecr.${defaults.awsRegion}.amazonaws.com/${defaults.projectName}/${defaults.projectName}:\$(echo $BRANCH_NAME)-\$(echo $BUILD_NUMBER)", label: "push image to registry"
+                if (env.BRANCH_NAME == "master") {
+                    sh script: "docker tag ${defaults.projectName} \$(aws sts get-caller-identity | jq -r .Account).dkr.ecr.${defaults.awsRegion}.amazonaws.com/${defaults.projectName}/${defaults.projectName}:latest", label: "tag image latest"
+                    sh script: "docker push \$(aws sts get-caller-identity | jq -r .Account).dkr.ecr.${defaults.awsRegion}.amazonaws.com/${defaults.projectName}/${defaults.projectName}:latest", label: "push latest image to registry"
+                }
+            }
         }
     }
 }
--- vars/eks_deploy.groovy
+++ vars/eks_deploy.groovy
@@ -1,46 +1,66 @@
 #!/usr/bin/env groovy
 
 def call(Map buildParams) {
-    if (!buildParams.hasProperty("nodeType") && !buildParams.nodeType) { buildParams.nodeType = "t2.medium" }
-    if (!buildParams.hasProperty("nodes") && !buildParams.nodes) { buildParams.nodes = 4 }
-    if (!buildParams.hasProperty("nodesMin") && !buildParams.nodesMin) { buildParams.nodesMin = buildParams.nodes }
-    if (!buildParams.hasProperty("nodesMax") && !buildParams.nodesMax) { buildParams.nodesMax = buildParams.nodes }
-    if (!buildParams.hasProperty("storageClass") && !buildParams.storageClass) { buildParams.storageClass = true }
-    if (!buildParams.hasProperty("eksParams") && !buildParams.eskParams) { buildParams.eksParams = "" }
-
-    node ( label: 'awscli' ) {
-        stage("checkout ${buildParams.env}") {
+    node ( label: 'linux' ) {
+        stage("gathering fatcs") {
             checkout scm
+            def env_files = findFiles(glob: '**/infrastructure/jenkins/*.y?ml') 
+            for (file in env_files) {
+                if (file[0].name == "defaults.yaml" ) {
+                    def defaults = readYaml file: "${file.path}"
+                    defaults.each { item -> 
+                        echo """${item}"""
+                    }
+                } else {
+                    def pipe_vars = readYaml file: "${file.path}"
+                    if (!pipe_vars.nodeType) { pipe_vars.nodeType = "t2.medium" }
+                    if (!pipe_vars.nodes) { pipe_vars.nodes = 4 }
+                    if (!pipe_vars.nodesMin) { pipe_vars.nodesMin = pipe_vars.nodes }
+                    if (!pipe_vars.nodesMax) { pipe_vars.nodesMax = pipe_vars.nodes }
+                    if (!pipe_vars.storageClass) { pipe_vars.storageClass = true }
+                    if (!pipe_vars.eksParams) { pipe_vars.eksParams = "" }
+                    if (pipe_vars.deploy) {
+                        pipe_vars.each { item ->
+                        node ( label: 'awscli' )
+                            stage("checkout ${pipe_vars.env}") {
+                                checkout scm
+                            }
+                            stage("create cluster ${pipe_vars.env}") {
+                                sh script: """\
+                                eksctl utils write-kubeconfig --name=${defaults.projectName}-${pipe_vars.env} --region=${defaults.awsRegion} \
+                                || eksctl create cluster \
+                                    --name=${defaults.projectName}-${pipe_vars.env} \
+                                    --region=${defaults.awsRegion} \
+                                    --nodes=${pipe_vars.nodes} \
+                                    --node-type=${pipe_vars.nodeType} \
+                                    --nodes-min=${pipe_vars.nodesMin} \
+                                    --nodes-max=${pipe_vars.nodesMax} \
+                                    --storage-class=${pipe_vars.storageClass} \
+                                    --alb-ingress-access \
+                                    ${pipe_vars.eksParams}""", label: "create cluster if not exist"
+                            }
+                            stage("deploy ${pipe_vars.env}") {
+                                sh script: "echo -e \"\\nProjectName: ${defaults.projectName}\" >> ./infrastructure/k8s/values.yaml", label: "building helm values - project name"
+                                sh script: "echo \"Env: ${pipe_vars.env}\" >> ./infrastructure/k8s/values.yaml", label: "building helm values - environment"
+                                sh script: "echo \"AwsRegion: ${defaults.awsRegion}\" >> ./infrastructure/k8s/values.yaml", label: "building helm values - AWS region"
+                                sh script: "echo \"BranchName: ${BRANCH_NAME}\" >> ./infrastructure/k8s/values.yaml", label: "building helm values - branch name"
+                                sh script: "echo \"BuildNumber: ${BUILD_NUMBER}\" >> ./infrastructure/k8s/values.yaml", label: "building helm values - build number"
+                                sh script: "echo \"Role: \$(if [ \$(kubectl get all | grep \"service/\${projectName}-service\" -c) -eq 0 ]; then echo blue; else if [ \$(kubectl describe service/\${projectName}-service | grep role=green -c) -eq 0 ]; then echo green; else echo blue; fi; fi)\" >> ./infrastructure/k8s/values.yaml", label: "building helm values - role"
+                                sh script: "helm template --values ./infrastructure/k8s/values.yaml --output-dir ./infrastructure/k8s/manifests ./infrastructure/k8s"
+                                sh script: "kubectl apply --recursive --filename ./infrastructure/k8s/manifests/kube/templates/deployment.yaml"
+                            }
+                            stage("integration tests ${pipe_vars.env}") {
+                                sh "sleep 90"
+                            }
+                            stage("promote ${pipe_vars.env}") {
+                                sh script: "kubectl apply --recursive --filename ./infrastructure/k8s/manifests/kube/templates/service.yaml"
+                            }
+                        }
+                    }
+                }
+            }
+
         }
-        stage("create cluster ${buildParams.env}") {
-            sh script: """\
-            eksctl utils write-kubeconfig --name=${projectName}-${buildParams.env} --region=${awsRegion} \
-            || eksctl create cluster \
-                --name=${projectName}-${buildParams.env} \
-                --region=${awsRegion} \
-                --nodes=${buildParams.nodes} \
-                --node-type=${buildParams.nodeType} \
-                --nodes-min=${buildParams.nodesMin} \
-                --nodes-max=${buildParams.nodesMax} \
-                --storage-class=${buildParams.storageClass} \
-                --alb-ingress-access \
-                ${buildParams.eksParams}""", label: "create cluster if not exist"
-        }
-        stage("deploy ${buildParams.env}") {
-            sh script: "echo -e \"\\nProjectName: ${projectName}\" >> ./infrastructure/k8s/values.yaml", label: "building helm values - project name"
-            sh script: "echo \"Env: ${buildParams.env}\" >> ./infrastructure/k8s/values.yaml", label: "building helm values - environment"
-            sh script: "echo \"AwsRegion: ${awsRegion}\" >> ./infrastructure/k8s/values.yaml", label: "building helm values - AWS region"
-            sh script: "echo \"BranchName: ${BRANCH_NAME}\" >> ./infrastructure/k8s/values.yaml", label: "building helm values - branch name"
-            sh script: "echo \"BuildNumber: ${BUILD_NUMBER}\" >> ./infrastructure/k8s/values.yaml", label: "building helm values - build number"
-            sh script: "echo \"Role: \$(if [ \$(kubectl get all | grep \"service/\${projectName}-service\" -c) -eq 0 ]; then echo blue; else if [ \$(kubectl describe service/\${projectName}-service | grep role=green -c) -eq 0 ]; then echo blue; else echo green; fi; fi)\" >> ./infrastructure/k8s/values.yaml", label: "building helm values - role"
-            sh script: "helm template --values ./infrastructure/k8s/values.yaml --output-dir ./infrastructure/k8s/manifests ./infrastructure/k8s"
-            sh script: "kubectl apply --recursive --filename ./infrastructure/k8s/manifests/kube/templates/deployment.yaml"
-        }
-        stage("integration tests ${buildParams.env}") {
-            sh "sleep 90"
-        }
-        stage("promote ${buildParams.env}") {
-            sh script: "kubectl apply --recursive --filename ./infrastructure/k8s/manifests/kube/templates/service.yaml"
-        }
+
     }
 }
