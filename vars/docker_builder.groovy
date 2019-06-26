bc8e3fc79e3c14680ba9a23f8e6e1108297028e7
bartlabno <labnob@gmail.com>
2019-06-26 16:24:52 +0100

    testing compose autobuilder

--- vars/docker_builder.groovy
+++ vars/docker_builder.groovy
@@ -11,22 +11,22 @@ def call(Map buildParams) {
             stage('test') {
                 sh script: "cp ~/scripts/unitTests.sh ."
                 sh script: "cp ~/scripts/Dockerfile.sdk-${defaults.sdkVersion} ./Dockerfile.test"
-                sh script: "docker build --no-cache -t ${defaults.projectName}-test -f Dockerfile.test .", label: "build test docker image"
-                sh script: "docker run ${defaults.projectName}-test", label: "run test docker image"
+                sh script: "docker build --no-cache -t ${defaults.projectName}-${defaults.applicationName}-test -f Dockerfile.test .", label: "build test docker image"
+                sh script: "docker run ${defaults.projectName}-${defaults.applicationName}-test", label: "run test docker image"
             }
             stage('build') {
                 parallel (
-                    "image build": { sh script: "docker build --no-cache -t ${defaults.projectName} -f Dockerfile .", label: "build image" },
+                    "image build": { sh script: "docker build --no-cache -t ${defaults.projectName}-${defaults.applicationName} -f Dockerfile .", label: "build image" },
                     "registry login": { sh script: "\$(aws ecr get-login --region ${defaults.awsRegion} --no-include-email)", label: "login to docker registry" }
                 )
             }
             stage('publish') {
-                sh script: "docker tag ${defaults.projectName} \$(aws sts get-caller-identity | jq -r .Account).dkr.ecr.${defaults.awsRegion}.amazonaws.com/${defaults.projectName}:\$(echo $BRANCH_NAME)-\$(echo $BUILD_NUMBER)", label: "tag image"
-                sh script: "(aws ecr list-images --region ${defaults.awsRegion} --repository-name ${defaults.projectName}) || (aws ecr create-repository --region ${defaults.awsRegion} --repository-name ${defaults.projectName})", label: "check if repository exist"
-                sh script: "docker push \$(aws sts get-caller-identity | jq -r .Account).dkr.ecr.${defaults.awsRegion}.amazonaws.com/${defaults.projectName}:\$(echo $BRANCH_NAME)-\$(echo $BUILD_NUMBER)", label: "push image to registry"
+                sh script: "docker tag ${defaults.projectName}-${defaults.applicationName} \$(aws sts get-caller-identity | jq -r .Account).dkr.ecr.${defaults.awsRegion}.amazonaws.com/${defaults.projectName}-${defaults.applicationName}:\$(echo $BRANCH_NAME)-\$(echo $BUILD_NUMBER)", label: "tag image"
+                sh script: "(aws ecr list-images --region ${defaults.awsRegion} --repository-name ${defaults.projectName}-${defaults.applicationName}) || (aws ecr create-repository --region ${defaults.awsRegion} --repository-name ${defaults.projectName}-${defaults.applicationName})", label: "check if repository exist"
+                sh script: "docker push \$(aws sts get-caller-identity | jq -r .Account).dkr.ecr.${defaults.awsRegion}.amazonaws.com/${defaults.projectName}-${defaults.applicationName}:\$(echo $BRANCH_NAME)-\$(echo $BUILD_NUMBER)", label: "push image to registry"
                 if (env.BRANCH_NAME == "master") {
-                    sh script: "docker tag ${defaults.projectName} \$(aws sts get-caller-identity | jq -r .Account).dkr.ecr.${defaults.awsRegion}.amazonaws.com/${defaults.projectName}:latest", label: "tag image latest"
-                    sh script: "docker push \$(aws sts get-caller-identity | jq -r .Account).dkr.ecr.${defaults.awsRegion}.amazonaws.com/${defaults.projectName}:latest", label: "push latest image to registry"
+                    sh script: "docker tag ${defaults.projectName}-${defaults.applicationName} \$(aws sts get-caller-identity | jq -r .Account).dkr.ecr.${defaults.awsRegion}.amazonaws.com/${defaults.projectName}-${defaults.applicationName}:latest", label: "tag image latest"
+                    sh script: "docker push \$(aws sts get-caller-identity | jq -r .Account).dkr.ecr.${defaults.awsRegion}.amazonaws.com/${defaults.projectName}-${defaults.applicationName}:latest", label: "push latest image to registry"
                 }
             }
         }
