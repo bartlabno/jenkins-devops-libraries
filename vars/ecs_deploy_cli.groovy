75685ce3a565038c4e061b39fb37efcc6e6d2442
bartlabno <labnob@gmail.com>
2019-06-27 15:04:00 +0100

    array to string

--- vars/ecs_deploy_cli.groovy
+++ vars/ecs_deploy_cli.groovy
@@ -24,7 +24,10 @@ def call(Map buildParams) {
                 pipe_vars.subnets.each { subnetX ->
                     sh "echo this subnet is ${subnetX}"
                 }
+
                 sh "echo list not as array is  ${pipe_vars.subnets.join(",")}"
+                sh "ok"
+                
                 exit 0
 
                 if (pipe_vars.deploy) {
