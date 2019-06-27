73c9987fd61d7381be42220a23b165b9cb583a0d
bartlabno <labnob@gmail.com>
2019-06-27 14:04:30 +0100

    fix to command line - fix quotes -debug 2

--- vars/ecs_deploy_cli.groovy
+++ vars/ecs_deploy_cli.groovy
@@ -10,17 +10,17 @@ def call(Map buildParams) {
                 if (!pipe_vars.memLimit) { pipe_vars.memLimit = "0.5" }
                 if (!pipe_vars.cpuLimit) { pipe_vars.cpuLimit = "256" }
                 if (!pipe_vars.create_vpc) { pipe_vars.create_vpc = false }
-                if (!pipe_vars.vpc) { 
+                if (!pipe_vars.vpc) {
                     sh "export DEFAULT_VPC=\$(aws ec2 describe-vpcs --region ${defaults.awsRegion} --filters Name=isDefault,Values=true --output text --query Vpcs[].VpcId)"
                     pipe_vars.vpc = $DEFAULT_VPC
                     sh "echo ${pipe_vars.vpc}"
                 }
                 if (!pipe_vars.subnets) { 
                     sh "export DEFAULT_SUBNETS=\"\$(aws ec2 describe-subnets --region ${defaults.awsRegion} --filters Name=vpc-id,Values=\$(aws ec2 describe-vpcs --region ${defaults.awsRegion} --filters Name=isDefault,Values=true --output text --query Vpcs[].VpcId) --output text --query Subnets[].SubnetId)\""
-                    pipe_vars.subnets = $DEFAULT_SUBNETS
                     sh "echo ${pipe_vars.vpc}"
                     sh "echo \$DEFAULT_SUBNETS"
                     sh "echo ${pipe_vars.subnets}"
+                    pipe_vars.subnets = $DEFAULT_SUBNETS
                 }
                 sh "echo ${pipe_vars.vpc} and ${pipe_vars.subnets}"
                 pipe_vars.subnets.each { subnetX ->
