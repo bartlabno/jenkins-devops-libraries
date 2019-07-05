7ff80330fd19cf4d52e8a04d5ce96f5efaa677d1
bartlabno <labnob@gmail.com>
2019-07-05 14:34:26 +0100

    fix to ecs deploy lib

--- vars/ecs_deploy.groovy
+++ vars/ecs_deploy.groovy
@@ -1,13 +1,15 @@
 #!/usr/bin/env groovy
 
-node ( label: 'linux' ) {
-    stage("deploy") {
-        checkout scm
-        def defaults = readYaml file: "./jenkins.yaml"
-        defaults.project_env.each { env, env_values ->
-            env_values.each { is_deploy, is_true ->
-                if (is_deploy == "deploy" && is_true) {
-                    sh "ansible-pull -U https://github.com/bartlabno/cloud-formation-ecs-fargate.git --extra-vars @${WORKSPACE}/jenkins.yaml --extra-vars \"current_env=${env}\" --extra-vars \"branch_name=${BRANCH_NAME}\" --extra-vars \"build_number=${BUILD_NUMBER}\" site.yaml"
+def call(Map buildParams) {
+    node ( label: 'linux' ) {
+        stage("deploy") {
+            checkout scm
+            def defaults = readYaml file: "./jenkins.yaml"
+            defaults.project_env.each { env, env_values ->
+                env_values.each { is_deploy, is_true ->
+                    if (is_deploy == "deploy" && is_true) {
+                        sh "ansible-pull -U https://github.com/bartlabno/cloud-formation-ecs-fargate.git --extra-vars @${WORKSPACE}/jenkins.yaml --extra-vars \"current_env=${env}\" --extra-vars \"branch_name=${BRANCH_NAME}\" --extra-vars \"build_number=${BUILD_NUMBER}\" site.yaml"
+                    }
                 }
             }
         }
