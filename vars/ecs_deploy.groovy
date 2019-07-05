f41194841893d6a23afb3b0309783ad567d9999b
bartlabno <labnob@gmail.com>
2019-07-05 10:17:10 +0100

    test

--- vars/ecs_deploy.groovy
+++ vars/ecs_deploy.groovy
@@ -7,9 +7,7 @@ def call(Map buildParams) {
             def defaults = readYaml file: "./jenkins.yaml"
             defaults.project_env.each { key, value ->
                 println key
-                if (value.contains("deploy")) { 
-                    println value
-                }
+                println value
             }
             sh "echo \"${defaults.project_env}\""
             // defaults.project_name { envs ->
