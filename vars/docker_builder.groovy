0588b26affe54ea413efd6c027cedcf7f631a7a4
bartlabno <labnob@gmail.com>
2019-07-05 14:31:56 +0100

    docker build to use new config file - jenkins.yaml

--- vars/docker_builder.groovy
+++ vars/docker_builder.groovy
@@ -4,29 +4,28 @@ def call(Map buildParams) {
     node ( label: 'linux' ) {
         stage('builder checkout') {
             checkout scm
-            def defaults = readYaml file: "./infrastructure/jenkins/defaults.yaml"
+            def defaults = readYaml file: "./jenkins.yaml"
             defaults.each { item -> 
-                echo """${item}"""
             }    
             stage('test') {
                 sh script: "cp ~/scripts/unitTests.sh ."
-                sh script: "cp ~/scripts/Dockerfile.sdk-${defaults.sdkVersion} ./Dockerfile.test"
-                sh script: "docker build --no-cache -t ${defaults.projectName}-${defaults.applicationName}-test -f Dockerfile.test .", label: "build test docker image"
-                sh script: "docker run ${defaults.projectName}-${defaults.applicationName}-test", label: "run test docker image"
+                sh script: "cp ~/scripts/Dockerfile.sdk-2.2 ./Dockerfile.test"
+                sh script: "docker build --no-cache -t ${defaults.project_name}-${defaults.application_name}-test -f Dockerfile.test .", label: "build test docker image"
+                sh script: "docker run ${defaults.project_name}-${defaults.application_name}-test", label: "run test docker image"
             }
             stage('build') {
                 parallel (
-                    "image build": { sh script: "docker build --no-cache -t ${defaults.projectName}-${defaults.applicationName} -f Dockerfile .", label: "build image" },
-                    "registry login": { sh script: "\$(aws ecr get-login --region ${defaults.awsRegion} --no-include-email)", label: "login to docker registry" }
+                    "image build": { sh script: "docker build --no-cache -t ${defaults.project_name}-${defaults.application_name} -f Dockerfile .", label: "build image" },
+                    "registry login": { sh script: "\$(aws ecr get-login --region ${defaults.project_region} --no-include-email)", label: "login to docker registry" }
                 )
             }
             stage('publish') {
-                sh script: "docker tag ${defaults.projectName}-${defaults.applicationName} \$(aws sts get-caller-identity | jq -r .Account).dkr.ecr.${defaults.awsRegion}.amazonaws.com/${defaults.projectName}-${defaults.applicationName}:\$(echo $BRANCH_NAME)-\$(echo $BUILD_NUMBER)", label: "tag image"
-                sh script: "(aws ecr list-images --region ${defaults.awsRegion} --repository-name ${defaults.projectName}-${defaults.applicationName}) || (aws ecr create-repository --region ${defaults.awsRegion} --repository-name ${defaults.projectName}-${defaults.applicationName})", label: "check if repository exist"
-                sh script: "docker push \$(aws sts get-caller-identity | jq -r .Account).dkr.ecr.${defaults.awsRegion}.amazonaws.com/${defaults.projectName}-${defaults.applicationName}:\$(echo $BRANCH_NAME)-\$(echo $BUILD_NUMBER)", label: "push image to registry"
+                sh script: "docker tag ${defaults.project_name}-${defaults.application_name} \$(aws sts get-caller-identity | jq -r .Account).dkr.ecr.${defaults.project_region}.amazonaws.com/${defaults.project_name}-${defaults.application_name}:\$(echo $BRANCH_NAME)-\$(echo $BUILD_NUMBER)", label: "tag image"
+                sh script: "(aws ecr list-images --region ${defaults.project_region} --repository-name ${defaults.project_name}-${defaults.application_name}) || (aws ecr create-repository --region ${defaults.project_region} --repository-name ${defaults.project_name}-${defaults.application_name})", label: "check if repository exist"
+                sh script: "docker push \$(aws sts get-caller-identity | jq -r .Account).dkr.ecr.${defaults.project_region}.amazonaws.com/${defaults.project_name}-${defaults.application_name}:\$(echo $BRANCH_NAME)-\$(echo $BUILD_NUMBER)", label: "push image to registry"
                 if (env.BRANCH_NAME == "master") {
-                    sh script: "docker tag ${defaults.projectName}-${defaults.applicationName} \$(aws sts get-caller-identity | jq -r .Account).dkr.ecr.${defaults.awsRegion}.amazonaws.com/${defaults.projectName}-${defaults.applicationName}:latest", label: "tag image latest"
-                    sh script: "docker push \$(aws sts get-caller-identity | jq -r .Account).dkr.ecr.${defaults.awsRegion}.amazonaws.com/${defaults.projectName}-${defaults.applicationName}:latest", label: "push latest image to registry"
+                    sh script: "docker tag ${defaults.project_name}-${defaults.application_name} \$(aws sts get-caller-identity | jq -r .Account).dkr.ecr.${defaults.project_region}.amazonaws.com/${defaults.project_name}-${defaults.application_name}:latest", label: "tag image latest"
+                    sh script: "docker push \$(aws sts get-caller-identity | jq -r .Account).dkr.ecr.${defaults.project_region}.amazonaws.com/${defaults.project_name}-${defaults.application_name}:latest", label: "push latest image to registry"
                 }
             }
         }
