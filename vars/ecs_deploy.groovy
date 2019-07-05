fd07a25ba1edc9a79dca0efa7d1777a628b49b04
bartlabno <labnob@gmail.com>
2019-07-05 10:04:40 +0100

    test

--- vars/ecs_deploy.groovy
+++ vars/ecs_deploy.groovy
@@ -5,35 +5,36 @@ def call(Map buildParams) {
         stage("gathering fatcs") {
             checkout scm
             def defaults = readYaml file: "./jenkins.yaml"
-            sh "echo \"${defaults.project_env[*]}\""
-            defaults.project_name { envs ->
-                sh "echo \"${envs}\""
-                // sh "echo \"${defaults.app_type}\""
-                // sh "echo \"${defaults.allowed_ip}\""
-                // sh "echo \"${defaults.project_name}\""
-                // if (!defaults.mem_limit) { defaults.mem_limit = "0.5" }
-                // if (!defaults.cpu_limit) { defaults.cpu_limit = "256" }
-                // if (!defaults.use_vpc) {
-                //     sh "echo \"\$(aws ec2 describe-vpcs --region ${defaults.project_region} --filters Name=isDefault,Values=true --output text --query Vpcs[].VpcId)\" > infrastructure/default_vpc"
-                //     defaults.vpc = readFile 'infrastructure/default_vpc'
-                // }
-                // if (!defaults.use_subnets) { 
-                //     sh "echo \"\$(aws ec2 describe-subnets --region ${defaults.project_region} --filters Name=vpc-id,Values=\$(aws ec2 describe-vpcs --region ${defaults.project_region} --filters Name=isDefault,Values=true --output text --query Vpcs[].VpcId) --output text --query Subnets[].SubnetId)\" > infrastructure/default_subnets"
-                //     defaults.subnets = readFile 'infrastructure/default_subnets'
-                //     defaults.subnets = defaults.subnets.split()
-                // }
-                // if (defaults.deploy) {
-                //     node ( label: 'awscli' ) {
-                //         stage("checkout ${envs}") {
-                //             checkout scm
-                //         }
-                //         if (defaults.is_frontend) {
-                //             stage("frontend configuration ${envs}") {
-                //             }
-                //         }
-                //     }
-                // }
-            }
+            defaults.each{println it}
+            sh "echo \"${defaults.project_env}\""
+            // defaults.project_name { envs ->
+            //     sh "echo \"${envs}\""
+            //     // sh "echo \"${defaults.app_type}\""
+            //     // sh "echo \"${defaults.allowed_ip}\""
+            //     // sh "echo \"${defaults.project_name}\""
+            //     // if (!defaults.mem_limit) { defaults.mem_limit = "0.5" }
+            //     // if (!defaults.cpu_limit) { defaults.cpu_limit = "256" }
+            //     // if (!defaults.use_vpc) {
+            //     //     sh "echo \"\$(aws ec2 describe-vpcs --region ${defaults.project_region} --filters Name=isDefault,Values=true --output text --query Vpcs[].VpcId)\" > infrastructure/default_vpc"
+            //     //     defaults.vpc = readFile 'infrastructure/default_vpc'
+            //     // }
+            //     // if (!defaults.use_subnets) { 
+            //     //     sh "echo \"\$(aws ec2 describe-subnets --region ${defaults.project_region} --filters Name=vpc-id,Values=\$(aws ec2 describe-vpcs --region ${defaults.project_region} --filters Name=isDefault,Values=true --output text --query Vpcs[].VpcId) --output text --query Subnets[].SubnetId)\" > infrastructure/default_subnets"
+            //     //     defaults.subnets = readFile 'infrastructure/default_subnets'
+            //     //     defaults.subnets = defaults.subnets.split()
+            //     // }
+            //     // if (defaults.deploy) {
+            //     //     node ( label: 'awscli' ) {
+            //     //         stage("checkout ${envs}") {
+            //     //             checkout scm
+            //     //         }
+            //     //         if (defaults.is_frontend) {
+            //     //             stage("frontend configuration ${envs}") {
+            //     //             }
+            //     //         }
+            //     //     }
+            //     // }
+            // }
         }
     }
 }
