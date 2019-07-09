872fecb899c99c44841f4cc9af1f0ad47879a33b
bartlabno <labnob@gmail.com>
2019-07-09 15:34:04 +0100

    deploy stage only on master branch

--- vars/ecs_deploy.groovy
+++ vars/ecs_deploy.groovy
@@ -2,13 +2,15 @@
 
 def call(Map buildParams) {
     node ( label: 'linux' ) {
-        stage("deploy") {
+        stage("gathering facts") {
             checkout scm
             def defaults = readYaml file: "./jenkins.yaml"
             defaults.project_env.each { env, env_values ->
                 env_values.each { is_deploy, is_true ->
-                    if (is_deploy == "deploy" && is_true) {
-                        sh "ansible-pull -U https://github.com/bartlabno/cloud-formation-ecs-fargate.git --extra-vars @${WORKSPACE}/jenkins.yaml --extra-vars \"current_env=${env}\" --extra-vars \"branch_name=${BRANCH_NAME}\" --extra-vars \"build_number=${BUILD_NUMBER}\" site.yaml"
+                    if (is_deploy == "deploy" && is_true && BRANCH_NAME == "master") {
+                        stage("deploy to ${env_values}") {
+                            sh "ansible-pull -U https://github.com/bartlabno/cloud-formation-ecs-fargate.git --extra-vars @${WORKSPACE}/jenkins.yaml --extra-vars \"current_env=${env}\" --extra-vars \"branch_name=${BRANCH_NAME}\" --extra-vars \"build_number=${BUILD_NUMBER}\" site.yaml"
+                        }
                     }
                 }
             }
