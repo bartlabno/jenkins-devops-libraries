1c434a33c6c7c7438929eebc923be8b3e80b3dda
bartlabno <labnob@gmail.com>
2019-07-11 16:45:31 +0100

    typo in loop

--- vars/docker_builder.groovy
+++ vars/docker_builder.groovy
@@ -12,7 +12,7 @@ def call(Map buildParams) {
                 sh script: "cp ~/scripts/Dockerfile.sdk-2.2 ./Dockerfile.test"
                 if (defaults.build_arg) {
                         defaults.build_arg.each { arg ->
-                            sh "echo \"${arg}=\$(aws secretsmanager get-secret-value --secret-id /${defaults.projectName}/docker/${defaults.applicationName}/${secret} --output text --query SecretString)\" >> ./docker-build.args"
+                            sh "echo \"${arg}=\$(aws secretsmanager get-secret-value --secret-id /${defaults.projectName}/docker/${defaults.applicationName}/${arg} --output text --query SecretString)\" >> ./docker-build.args"
                     }
                     sh script: "docker build --no-cache -t ${defaults.project_name}-${defaults.application_name}-test \$(for i in 'cat docker-build.args'; do out+=\"--build-arg \$i \" ; done; echo \$out;out=\"\") -f Dockerfile.test  .", label: "build test docker image"
                 } else {
