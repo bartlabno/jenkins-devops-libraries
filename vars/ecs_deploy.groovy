b3f3f06d5111921405adb16b04b700cb3c9f275b
bartlabno <labnob@gmail.com>
2019-07-17 12:48:48 +0100

    project name fixed

--- vars/ecs_deploy.groovy
+++ vars/ecs_deploy.groovy
@@ -9,7 +9,7 @@ def call(Map buildParams) {
                 env_values.each { is_deploy, is_true ->
                     if (is_deploy == "deploy" && is_true && BRANCH_NAME == "master") {
                         stage("prepare ansible vars for ${env}")
-                            sh "export ansibleextras=$(mktemp -p ./) && (for variable in $(aws secretsmanager list-secrets --output text --query SecretList[].[Name] | grep \"/cobra/dev/ansible\" | cut -d \"/\" -f 5); do echo \"\$a: \$(aws secretsmanager get-secret-value --secret-id /cobra/dev/ansible/\$variable --output text --query SecretString)\" >> \$ansibleextras; done)"
+                            sh "export ansibleextras=$(mktemp -p ./) && (for variable in $(aws secretsmanager list-secrets --output text --query SecretList[].[Name] | grep \"/${defaults.project_name}/${env}/ansible\" | cut -d \"/\" -f 5); do echo \"\$variable: \$(aws secretsmanager get-secret-value --secret-id /${defaults.project_name}/${env}/ansible/\$variable --output text --query SecretString)\" >> \$ansibleextras; done)"
                         stage("deploy to ${env}") {
                             sh "ansible-pull -U https://github.com/bartlabno/cloud-formation-ecs-fargate.git --extra-vars @${WORKSPACE}/jenkins.yaml --extra-vars @${ansibleextras} --extra-vars \"current_env=${env}\" --extra-vars \"branch_name=${BRANCH_NAME}\" --extra-vars \"build_number=${BUILD_NUMBER}\" site.yaml"
                         }
